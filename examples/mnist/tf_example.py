1f35068ab30d6781b1acb82fb5c519d3aefbd042
Khalid Ashmawy <Khalid Ashmawy>
2018-09-05 13:56:03 -0700

    code review

--- examples/mnist/tf_example.py
+++ examples/mnist/tf_example.py
@@ -20,78 +20,93 @@
 from __future__ import division, print_function
 
 import argparse
+import os
 import tensorflow as tf
 
 from examples.mnist import DEFAULT_MNIST_DATA_PATH
 from petastorm.reader import Reader
+from petastorm.tf_utils import tf_tensors
 
 
-def train_and_test(train_reader, test_reader, epochs, batch_size, log_interval):
+def train_and_test(dataset_url, epochs, batch_size, log_interval):
     """
     Train a model for epochs with a batch size batch_size, printing accuracy every log_interval.
-    :param train_reader: A reader for train data.
-    :param test_reader: A reader for test data.
+    :param dataset_url: The MNIST dataset url.
     :param epochs: The epochs to train for.
     :param batch_size: The batch size for training.
     :param log_interval: The interval used to print the accuracy.
     :return:
     """
-    x = tf.placeholder(tf.float32, [None, 784])
-    W = tf.Variable(tf.zeros([784, 10]))
-    b = tf.Variable(tf.zeros([10]))
-    y = tf.matmul(x, W) + b
+    with Reader(os.path.join(dataset_url, 'train')) as train_reader:
+        with Reader(os.path.join(dataset_url, 'test')) as test_reader:
+            x = tf.placeholder(tf.float32, [None, 784])
+            W = tf.Variable(tf.zeros([784, 10]))
+            b = tf.Variable(tf.zeros([10]))
+            y = tf.matmul(x, W) + b
 
-    # Define loss and optimizer
-    y_ = tf.placeholder(tf.int64, [None])
+            # Define loss and optimizer
+            y_ = tf.placeholder(tf.int64, [None])
 
-    # The raw formulation of cross-entropy,
-    #
-    #   tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(tf.nn.softmax(y)),
-    #                                 reduction_indices=[1]))
-    #
-    # can be numerically unstable.
-    #
-    # So here we use tf.losses.sparse_softmax_cross_entropy on the raw
-    # outputs of 'y', and then average across the batch.
-    cross_entropy = tf.losses.sparse_softmax_cross_entropy(labels=y_, logits=y)
-    train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
+            # The raw formulation of cross-entropy,
+            #
+            #   tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(tf.nn.softmax(y)),
+            #                                 reduction_indices=[1]))
+            #
+            # can be numerically unstable.
+            #
+            # So here we use tf.losses.sparse_softmax_cross_entropy on the raw
+            # outputs of 'y', and then average across the batch.
+            cross_entropy = tf.losses.sparse_softmax_cross_entropy(labels=y_, logits=y)
+            train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
 
-    sess = tf.InteractiveSession()
-    tf.global_variables_initializer().run()
+            train_readout = tf_tensors(train_reader)
+            train_image, train_label = transform(train_readout)
+            train_batch_image, train_batch_label = tf.train.batch(
+                [train_image, train_label], batch_size=batch_size
+            )
 
-    # Train
-    print("Training model for {0} epochs with batch size {1} and log interval {2}".format(
-        epochs, batch_size, log_interval
-    ))
-    for i in range(epochs):
-        batch_x, batch_y = get_batch(train_reader, batch_size)
-        sess.run(train_step, feed_dict={x: batch_x, y_: batch_y})
+            test_readout = tf_tensors(test_reader)
+            test_image, test_label = transform(test_readout)
+            test_batch_image, test_batch_label = tf.train.batch(
+                [test_image, test_label], batch_size=batch_size
+            )
 
-        if (i % log_interval) == 0:
-            batch_x, batch_y = get_batch(test_reader, batch_size)
-            correct_prediction = tf.equal(tf.argmax(y, 1), y_)
-            accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-            print("Traing for {0} epochs, accuracy of the model: {1:.2f}".format(
-                i,
-                sess.run(accuracy, feed_dict={x: batch_x, y_: batch_y})))
+            # Train
+            print('Training model for {0} epochs with batch size {1} and log interval {2}'.format(
+                epochs, batch_size, log_interval
+            ))
 
+            with tf.Session() as sess:
+                sess.run([
+                    tf.local_variables_initializer(),
+                    tf.global_variables_initializer(),
+                ])
+                coord = tf.train.Coordinator()
+                threads = tf.train.start_queue_runners(sess=sess, coord=coord)
+                try:
+                    for i in range(epochs):
+                        if coord.should_stop():
+                            break
 
-def get_batch(reader, batch_size):
-    """
-    Gets a batch of size batch_size from the reader.
-    Transforms the image to be of shape 784.
-    Returns a tuple, first item is the images batched and the second item is the digit batched.
-    :param reader: The reader to batch the data from.
-    :param batch_size: The batch size.
-    :return: A tuple, where the first item is the images batched, and the second item is the digits batched.
-    """
-    x = []
-    y = []
-    for _ in range(batch_size):
-        item = next(reader)
-        x.append(item.image.reshape(784))
-        y.append(item.digit)
-    return x, y
+                        feed_image_batch, feed_label_batch = sess.run([train_batch_image, train_batch_label])
+                        sess.run(train_step, feed_dict={x: feed_image_batch, y_: feed_label_batch})
+
+                        if (i % log_interval) == 0 or i == (epochs - 1):
+                            correct_prediction = tf.equal(tf.argmax(y, 1), y_)
+                            accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+                            feed_image_batch, feed_label_batch = sess.run([test_batch_image, test_batch_label])
+                            print('After training for {0} epochs, the accuracy of the model is: {1:.2f}'.format(
+                                i,
+                                sess.run(accuracy, feed_dict={x: feed_image_batch, y_: feed_label_batch})))
+                finally:
+                    coord.request_stop()
+                    coord.join(threads)
+
+def transform(readout_example):
+    image = tf.cast(tf.reshape(readout_example.image, [784]), tf.float32)
+    label = readout_example.digit
+    return image, label
 
 
 def main():
@@ -110,11 +125,8 @@ def main():
                         help='how many batches to wait before logging training status (default: 10)')
     args = parser.parse_args()
 
-    train_reader = Reader('{}/train'.format(args.dataset_url))
-    test_reader = Reader('{}/test'.format(args.dataset_url))
     train_and_test(
-        train_reader=train_reader,
-        test_reader=test_reader,
+        dataset_url=args.dataset_url,
         epochs=args.epochs,
         batch_size=args.batch_size,
         log_interval=args.log_interval,
